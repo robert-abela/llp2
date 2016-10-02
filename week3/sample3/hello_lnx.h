a99a2599fd5af0b3f8bc8263e0efcd2a32d5f95d
Robert Abela <robert.abela@mcast.edu.mt>
2016-10-02 07:00:17 +0200

    added include once for headers

--- week3/sample3/hello_lnx.h
+++ week3/sample3/hello_lnx.h
@@ -1,6 +1,11 @@
+#ifndef HELLO_LNX_H_INCLUDED
+#define HELLO_LNX_H_INCLUDED
+
 #include <stdio.h>
 
 void hello()
 {
   printf ("Hello Linux!\n");
 }
+
+#endif //HELLO_LNX_H_INCLUDED
--- week3/sample3/hello_win.h
+++ week3/sample3/hello_win.h
@@ -1,6 +1,10 @@
+#ifndef HELLO_WIN_H_INCLUDED
+#define HELLO_WIN_H_INCLUDED
 #include <stdio.h>
 
 void hello()
 {
   printf ("Hello Windows!\n");
 }
+
+#endif //HELLO_WIN_H_INCLUDED
