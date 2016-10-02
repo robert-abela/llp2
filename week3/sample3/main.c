4f2d60b3c4fb1e2104c14ff8a29a5a5efdc16ccf
Robert Abela <robert.abela@mcast.edu.mt>
2016-10-02 06:50:54 +0200

    added multi-target build

---
+++ week3/sample3/main.c
@@ -0,0 +1,13 @@
+#if defined WIN32
+   #include "hello_win.h"
+#elif defined LINUX
+   #include "hello_lnx.h"
+#else
+   #error only windows and linux supported
+#endif
+
+int main (void)
+{
+  hello();
+  return 0;
+}
