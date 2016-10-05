837ff4bbd3eb3a47b83a2cb9094858d5603f55b3
Robert Abela <robert.abela@mcast.edu.mt>
2016-10-05 05:44:48 +0200

    adding resources for worksheet1

---
+++ worksheets/worksheet1/worksheet1.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#define MESSAGE "Most people's C programs should be indented six feet downward and covered with dirt."
+int main(void)
+{
+	/* Blair P. Houghton said the following */
+	printf(MESSAGE);
+	return 0;
+}
---
+++ worksheets/worksheet1/worksheet1.s
@@ -0,0 +1,38 @@
+	.file	"worksheet1.c"
+	.section	.rodata
+	.align 4
+.LC0:
+	.string	"most people's C programs should be indented six feet downward and covered with dirt."
+	.text
+	.globl	main
+	.type	main, @function
+main:
+.LFB0:
+	.cfi_startproc
+	leal	4(%esp), %ecx
+	.cfi_def_cfa 1, 0
+	andl	$-16, %esp
+	pushl	-4(%ecx)
+	pushl	%ebp
+	.cfi_escape 0x10,0x5,0x2,0x75,0
+	movl	%esp, %ebp
+	pushl	%ecx
+	.cfi_escape 0xf,0x3,0x75,0x7c,0x6
+	subl	$4, %esp
+	subl	$12, %esp
+	pushl	$.LC0
+	call	printf
+	addl	$16, %esp
+	movl	$0, %eax
+	movl	-4(%ebp), %ecx
+	.cfi_def_cfa 1, 0
+	leave
+	.cfi_restore 5
+	leal	-4(%ecx), %esp
+	.cfi_def_cfa 4, 4
+	ret
+	.cfi_endproc
+.LFE0:
+	.size	main, .-main
+	.ident	"GCC: (Ubuntu 5.4.0-6ubuntu1~16.04.2) 5.4.0 20160609"
+	.section	.note.GNU-stack,"",@progbits
