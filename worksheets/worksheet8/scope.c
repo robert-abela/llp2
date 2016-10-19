9500ca5e8bf3c7207f79e96cd431a6abf50c4068
Robert (desktop) <robert.abela.15@um.edu.mt>
2016-10-19 22:13:25 +0200

    adding code sample for worksheet8

---
+++ worksheets/worksheet8/scope.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+char* pointer_stack;
+char* pointer_heap;
+
+void init_pointers()
+{
+	char local_array[100];
+	pointer_stack = local_array;
+	pointer_heap = malloc(100);
+
+	strcpy(pointer_heap, "testString");
+	strcpy(pointer_stack, "testString");
+}
+
+void print_pointers()
+{
+	//char local_array[100] = {0};
+
+	printf("pointer_heap:  %s\n", pointer_heap);
+	printf("pointer_stack: %s\n", pointer_stack);
+}
+
+int main(void)
+{
+	init_pointers();
+	print_pointers();
+
+	free(pointer_heap);
+	return 0;
+}
