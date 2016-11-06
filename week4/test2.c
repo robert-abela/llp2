97be910e5db4b30cae1223666ef8d119b5121055
Robert (desktop) <robert.abela.15@um.edu.mt>
2016-11-06 23:51:24 +0100

    final modifications to second sample in wk4

--- week4/test2.c
+++ week4/test2.c
@@ -1,16 +1,24 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main(int argc, char **argv)
+char c;
+
+void do_user_input()
 {
 	char* buffer = (char*)malloc(128);
 	int number = 0;
-	char c = 0;
+
 	printf("Enter a word: ");
 	scanf("%s", buffer);
 	printf("Enter a number: ");
 	scanf("%d", &number);
 	c = buffer[number];
 	printf("Character at %d = %c: \n", number, c);
+	free(buffer);
+}
+
+int main(int argc, char **argv)
+{
+	
 	return 0;
 }
