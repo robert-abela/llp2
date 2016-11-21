f7bc82fa652777b41d0c79c692e7e39333ad08b4
Robert (desktop) <robert.abela.15@um.edu.mt>
2016-11-21 21:19:16 +0100

    formatting..

--- week9/thread_example.c
+++ week9/thread_example.c
@@ -2,31 +2,31 @@
 #include <unistd.h>
 #include <pthread.h>
 
-void *background_task(void *argument)
-{ 
+void* background_task(void *ptr)
+{
 	printf("Printing from the thread!\n");
-  	return (NULL);
+	return NULL;
 }
 
-int main()
-{ 
+int main(void)
+{
 	pthread_t 	mythread;
-  	int			result;
+	int			result;
 
-  	if (sysconf(_SC_THREADS) == -1)
-  	{
-  		fprintf(stderr, "Sorry, this program requires threads, which aren't supported here.\n");
-    	return -1;
-   	}
+	if (sysconf(_SC_THREADS) == -1)
+	{
+		fprintf(stderr, "Sorry, this program requires threads, which aren't supported here.\n");
+		return -1;
+	}
 
 	result = pthread_create(&mythread, NULL, background_task, NULL);
 
-  	if ( result != 0 )
-    { 
-    	fprintf(stderr, "For some reason I couldn't create a thread! (errno = %d)\n",result);
-       	return -2;
-    }
+	if (result != 0)
+	{
+		fprintf(stderr, "For some reason I couldn't create a thread! (errno = %d)\n", result);
+		return -2;
+	}
 
-  	pthread_join(mythread, NULL);
-  	return 0;
+	pthread_join(mythread, NULL);
+	return 0;
 }
