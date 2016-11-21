8c29e24fda96b269a4a73186e4015f96355fc320
Robert (desktop) <robert.abela.15@um.edu.mt>
2016-11-21 21:12:03 +0100

    added thread params example

---
+++ week9/thread_params_example.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <unistd.h> /* sleep */
+#include <pthread.h>
+
+void* print_msg(void *ptr)
+{
+	sleep(5); // seconds
+	printf("Message from thread: %s \n", (char*)ptr);
+	return ptr;
+}
+
+int main(void)
+{
+	pthread_t t1, t2;
+	char *msg1 = "Thread One", *msg2 = "Thread Two";
+
+	// Create 2 threads, passings the strings as params
+	int r1 = pthread_create(&t1, NULL, print_msg, (void*) msg1);
+	int r2 = pthread_create(&t2, NULL, print_msg, (void*) msg2);
+
+	printf("Thread 1 %s\n", r1==0 ? "created" : "failed");
+	printf("Thread 2 %s\n", r2==0 ? "created" : "failed");
+
+	// Wait till threads are complete.
+	pthread_join(t1, NULL);
+	pthread_join(t2, NULL); 
+
+	return 0;
+}
