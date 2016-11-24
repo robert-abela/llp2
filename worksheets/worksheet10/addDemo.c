58025af0b3b576397466a01ceb42b1313336e0f2
Robert (desktop) <robert.abela.15@um.edu.mt>
2016-11-24 22:58:22 +0100

    adding files for static/dynamic linking

---
+++ worksheets/worksheet10/addDemo.c
@@ -0,0 +1,9 @@
+#include <add.h>
+#include <stdio.h>
+ 
+int main()
+{
+  int x= 10, y = 20;
+  printf("\n%d + %d = %d", x, y, add(x, y));
+  return 0;
+}
---
+++ worksheets/worksheet10/addDemo2.c
@@ -0,0 +1,10 @@
+#include <heymath.h>
+#include <stdio.h>
+ 
+int main()
+{
+	int x = 10, y = 20;
+	printf("\n%d + %d = %d", x, y, add(x, y));
+	printf("\n%d + %d = %d", x, y, sub(x, y));
+	return 0;
+}
