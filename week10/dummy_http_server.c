a284119cdaeeb72c7b139d373b534e7eda79d95c
Robert (desktop) <robert.abela.15@um.edu.mt>
2016-11-27 09:24:28 +0100

    changed to use only loopback (to avoid firewall permissions)

--- week10/dummy_http_server.c
+++ week10/dummy_http_server.c
@@ -20,7 +20,7 @@ int main( int argc, char *argv[] )
 	/* Initialize socket structure (sockarrd_in) */
 	memset(&serv_addr, 0, sizeof(serv_addr));
 	serv_addr.sin_family = AF_INET;
-	serv_addr.sin_addr.s_addr = INADDR_ANY;
+	serv_addr.sin_addr.s_addr = htonl(INADDR_LOOPBACK); //INADDR_ANY
 	serv_addr.sin_port = htons(HTTP_PORT);
 
 	/* Bind the host address */
