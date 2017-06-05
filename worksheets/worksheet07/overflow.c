2d24434ad7160763049242ef0548b46fcdd8143d
Robert Abela <robert.abela@mcast.edu.mt>
2017-06-05 19:17:55 +0200

    updated code for ws7 to use gcc SPP

--- worksheets/worksheet07/overflow.c
+++ worksheets/worksheet07/overflow.c
@@ -1,20 +1,23 @@
 #include <stdio.h>
 #include <string.h>
 
+#define TRUE  1
+#define FALSE 0
+
 int main(void)
 {
 	char buffer[11];
-	int correct_password = 0;
+	int correct_password = FALSE;
 	
 	printf("Enter password: ");
 	gets(buffer);
 	
-	if (strcmp(buffer, "letmein"))
-		printf ("Incorrect Password!\n");
+	if (strcmp(buffer, "letmein") == 0)
+		correct_password = TRUE;
 	else
-		correct_password = 1;
+		printf ("Incorrect Password!\n");
 	
-	if (correct_password != 0)
+	if (correct_password != FALSE)
 		printf ("Performing tasks that require password...\n");
 
 	return 0;
