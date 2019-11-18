c1ee587922ac5c483e21cdb8ea0aef4b78942237
Robert Abela <robert-abela@users.noreply.github.com>
2019-11-18 18:26:43 +0100

    Update filewrite.c

--- week08/filewrite.c
+++ week08/filewrite.c
@@ -1,14 +1,14 @@
 #include <stdio.h>
 
-int main(void)
-{
+int main(void) {
 	char *string = "this is just a chunk of characters.";
 	FILE *fp;
 
 	fp = fopen("out.txt", "w");
-	if (fp != NULL)
-	{
+	if (fp != NULL) {
 		fprintf(fp, "%s\n", string);
 		fclose(fp);
 	}
+	
+	return 0;
 }
