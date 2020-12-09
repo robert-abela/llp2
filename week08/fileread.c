0abc38c416958b14aadfecf6c312eb6a4b58677d
llp2 <robert.abela@mcast.edu.mt>
2020-12-09 13:50:08 +0100

    update makefile and add comments

--- week08/fileread.c
+++ week08/fileread.c
@@ -7,9 +7,11 @@ int main(void) {
 
 	fp = fopen("file.txt", "r");
 	if (fp != NULL) {
+		//file was opened
 		while (!feof(fp)) {
+			//there is still something left to read
 			if (fscanf(fp, "%s %d", string, &num) != 2)
-				break;
+				break; //the file format is not as i expected
 			printf("Read: %s\t%d\n", string, num);
 		}
 
