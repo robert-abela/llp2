996b55bbb1fa3498595912f3551d20936855bff5
llp2 <robert.abela@mcast.edu.mt>
2020-12-17 07:51:40 +0100

    added alternative localhost addr

--- week10/simple_client.c
+++ week10/simple_client.c
@@ -21,7 +21,8 @@ int main(int argc, char *argv[]) {
 
 	memset(&srv_addr, 0, sizeof(srv_addr));
 	srv_addr.sin_family = AF_INET;      /* host byte order */
-	srv_addr.sin_addr.s_addr = inet_addr("127.0.0.1");
+	//srv_addr.sin_addr.s_addr = inet_addr("127.0.0.1");
+   	srv_addr.sin_addr.s_addr = htonl(INADDR_LOOPBACK);
     srv_addr.sin_port = htons(PORT);    /* short, network byte order */
 
     if (connect(sockfd, (struct sockaddr *)&srv_addr, sizeof(srv_addr)) == -1) {
