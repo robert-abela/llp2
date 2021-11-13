7f2485d4b0d6332d7fa779a723214fdb961bdf42
Robert Abela <robert-abela@users.noreply.github.com>
2021-11-13 21:14:24 +0100

    Add files via upload

---
+++ worksheets/worksheet15/leaks.c
@@ -0,0 +1,14 @@
+#include <stdlib.h>
+
+int main (void) {
+    void * pointers[9];
+    pointers[0] = malloc(1024); // this allocation not freed!
+
+    for (int i = 1; i < 9; i++) // alloc 8 buffers
+        pointers[i] = malloc(128);
+
+    for (int i = 1; i < 9; i++) 
+        free(pointers[i]); // free 8 buffers
+    
+    return 0;
+}
