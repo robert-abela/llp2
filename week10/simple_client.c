797c3fef49ad52443c5920bc8d2dd3fdc75f2d99
Robert Abela <robert-abela@users.noreply.github.com>
2022-11-02 05:41:48 +0100

    fix tabs->spaces

--- week10/simple_client.c
+++ week10/simple_client.c
@@ -19,9 +19,9 @@ int main(int argc, char *argv[]) {
         return -1;
     }
 
-	srv_addr.sin_family = AF_INET;      /* host byte order */
-	//srv_addr.sin_addr.s_addr = inet_addr("127.0.0.1");
-   	srv_addr.sin_addr.s_addr = htonl(INADDR_LOOPBACK);
+    srv_addr.sin_family = AF_INET;      /* host byte order */
+    //srv_addr.sin_addr.s_addr = inet_addr("127.0.0.1");
+    srv_addr.sin_addr.s_addr = htonl(INADDR_LOOPBACK);
     srv_addr.sin_port = htons(PORT);    /* short, network byte order */
 
     if (connect(sockfd, (struct sockaddr *)&srv_addr, sizeof(srv_addr)) == -1) {
