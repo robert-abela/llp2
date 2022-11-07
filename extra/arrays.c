6861fc270a369f28d369dde1aecd7c67c33f9a0f
robert <robert.abela@mcast.edu.mt>
2022-11-07 16:00:26 +0100

    comit class changes

---
+++ extra/arrays.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main(int argc, char* argv[]) {
+
+    char array_init[] = "abcd";
+    printf("%c",   array_init[0]);
+    printf("%c",   array_init[1]);
+    printf("%c",   array_init[2]);
+    printf("%c\n", array_init[3]);
+    
+    char array_uninit[5];
+    char* test = array_uninit;
+    strcpy(test, "bye");
+    printf("%c\n", array_uninit[0]);
+    printf("%c", *test);
+    printf("%c", *(test+1));
+    printf("%c\n", *(test+2));
+
+    char* array_heap = malloc(sizeof(char) * 5);
+    printf("%c\n", array_heap[0]);
+
+    return 0;
+}
